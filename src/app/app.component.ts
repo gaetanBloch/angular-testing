14556a50e7e5061468fb868987828e250e2a5747
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-25 22:35:56 +0200

    Made the original tests work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-testing app is running!';
+  title = 'angular-testing';
 }
