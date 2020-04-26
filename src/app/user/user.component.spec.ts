56088a49528996a3cbd910b6af27602a68898a72
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-26 10:34:33 +0200

    Prepare the User Component test environment

---
+++ src/app/user/user.component.spec.ts
@@ -0,0 +1,13 @@
+import { async, TestBed } from '@angular/core/testing';
+
+import { UserComponent } from './user.component';
+
+describe('UserComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        UserComponent
+      ],
+    }).compileComponents();
+  }));
+});
