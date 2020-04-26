d3ead2882daf6d9c0d2947379c171d76a7869513
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-26 11:35:58 +0200

    Added Reverse Pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
+import { ReversePipe } from './shared/reverse.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    ReversePipe
   ],
   imports: [
     BrowserModule
