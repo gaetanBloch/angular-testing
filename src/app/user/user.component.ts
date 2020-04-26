d712d235d38871f68e976953c7a809dc82b13030
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-04-26 10:45:59 +0200

    Added UserService to provide the user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from './user.service';
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -9,9 +11,10 @@ export class UserComponent implements OnInit {
   user: {name: string};
   isLoggedIn = false;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.user = this.userService.user;
   }
 
 }
